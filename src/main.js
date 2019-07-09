22c7cb4c73003ee9f1be2cb20bba9a6cd7ced998
Emerson Jordan and Holden Clark <holden@>
2019-07-09 15:43:56 -0700

    add post and get for groceryUsers

--- src/main.js
+++ src/main.js
@@ -3,11 +3,33 @@ import './styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Example } from './backend-code';
+import axios from "axios";
 
 let example = new Example();
 
 $(document).ready(function() {
-  $("#test").append(example.name);
-  example.exampleMethod();
-  $("#test").append(example.name);
+
+  $("#testSubmit").click(function(){
+    let userName = "Fred";
+    let x = $("#testUser").val();
+    try {
+      const params = {
+        "userName": "Fred"
+      }
+      axios.post(`https://r11ze6nefi.execute-api.us-west-2.amazonaws.com/test/groceryusers/${userName}`, params );
+    } catch (err) {
+      $("#test").append(`An error occured: ${err}`);
+    }
+
+    function getUsers() {
+      axios.get(`https://r11ze6nefi.execute-api.us-west-2.amazonaws.com/test/groceryusers`)
+        .then(response => {
+          console.log(response);
+          $("#test").append(`<br>${response.data[x].userName}`);
+        });
+    }
+
+    getUsers();
+
+    });
 });
